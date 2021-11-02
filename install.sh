f3b5d35c4c1fe9a3a0805565729a84a7d572732d
Areno <90553873+areno-dev@users.noreply.github.com>
2021-11-02 09:49:55 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+echo "Starting Installation..."
+mkdir -p dlib
+git clone -b 'v19.6' --single-branch https://github.com/davisking/dlib.git dlib/
+cd ./dlib
+sudo apt-get install cmake
+mkdir build; cd build; cmake .. ; cmake --build 
+
+pip3 install dlib
+
+pip3 install numpy
+pip3 install scikit-image
+sudo apt-get install python3-scipy
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libqtgui4
+sudo apt-get install python3-pyqt5
+sudo apt install libqt4-test
+pip3 install opencv-python==3.4.6.27
+pip3 install face_recognition 
