875e8ea35b2ad31b2a32f2c29c25bc58f592523f
Areno <90553873+areno-dev@users.noreply.github.com>
2021-11-25 09:06:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo "Starting Installation..."
+cd
 mkdir -p dlib
 git clone -b 'v19.6' --single-branch https://github.com/davisking/dlib.git dlib/
 cd ./dlib
@@ -15,5 +16,5 @@ sudo apt-get install libjasper-dev
 sudo apt-get install libqtgui4
 sudo apt-get install python3-pyqt5
 sudo apt install libqt4-test
-pip3 install opencv-python==3.4.6.27
+pip3 install opencv-python
 pip3 install face_recognition 
